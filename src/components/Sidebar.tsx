61e76e62adca804705230aa2803cbb48d45da855
tinoe0404 <tinoe044@gmail.com>
2025-10-28 14:20:08 +0200

    UnAuthenticatedSidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { currentUser } from "@clerk/nextjs/server";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
-const Sidebar = () => {
+async function Sidebar() {
+  const authUser = await currentUser();
+  if (!authUser) return <UnAuthenticatedSidebar />;
+
+
+
   return (
     <div>Sidebar</div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
+
+const UnAuthenticatedSidebar = () => (
+  <div className="sticky top-20">
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-center text-xl font-semibold">Welcome Back!</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="text-center text-muted-foreground mb-4">
+          Login to access your profile and connect with others.
+        </p>
+        <SignInButton mode="modal">
+          <Button className="w-full" variant="outline">
+            Login
+          </Button>
+        </SignInButton>
+        <SignUpButton mode="modal">
+          <Button className="w-full mt-2" variant="default">
+            Sign Up
+          </Button>
+        </SignUpButton>
+      </CardContent>
+    </Card>
+  </div>
+);
