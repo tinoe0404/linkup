ce3f51a8c8ecf7a00de1ff06f2caf0489427cae6
tinoe0404 <tinoe044@gmail.com>
2025-10-29 15:41:28 +0200

    updated the page.tsx

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -2,6 +2,7 @@
 
 import prisma from "@/lib/prisma";
 import { auth, currentUser} from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 export async function syncUser() {
     try {
