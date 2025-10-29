26306286d9144f43444ac61f25fe8703013d9b28
tinoe0404 <tinoe044@gmail.com>
2025-10-29 09:24:54 +0200

    created a creating post ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+import CreatePost from "@/components/CreatePost";
+import PostCard from "@/components/PostCard";
+import WhoToFollow from "@/components/WhoToFollow";
+import { currentUser } from "@clerk/nextjs/server";
+
 export default async function Home() {
+  const user = await currentUser();
 
   return (
-    <div className="m-4">
-      <h1>Home Pag content</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
+        </div>
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </div>
     </div>
   );
 }
