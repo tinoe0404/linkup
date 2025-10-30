b816bae8281437739173e755a0afd36e6968cb96
tinoe0404 <tinoe044@gmail.com>
2025-10-30 15:37:51 +0200

    create getNotificationIcon function

--- src/components/NotificationSkeleton.tsx
+++ src/components/NotificationSkeleton.tsx
@@ -14,6 +14,19 @@ import toast from "react-hot-toast";
 type Notifications = Awaited<ReturnType<typeof getNotifications>>;
 type Notification = Notifications[number];
 
+const getNotificationIcon = (type: string) => {
+  switch (type) {
+    case "LIKE":
+      return <HeartIcon className="size-4 text-red-500" />;
+    case "COMMENT":
+      return <MessageCircleIcon className="size-4 text-blue-500" />;
+    case "FOLLOW":
+      return <UserPlusIcon className="size-4 text-green-500" />;
+    default:
+      return null;
+  }
+};
+
 
 function NotificationsPage() {
   const [notifications, setNotifications] = useState<Notification[]>([]);
