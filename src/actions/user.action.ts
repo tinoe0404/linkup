12e8c26d3c6c65efd42ed7ba3bc37ef3d59e1256
tinoe0404 <tinoe044@gmail.com>
2025-10-30 10:12:49 +0200

    feat(post): add state management for likes and comments

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -1,8 +1,8 @@
 "use server"
 
-import prisma from "@/lib/prisma";
-import { auth, currentUser} from "@clerk/nextjs/server";
-import { revalidatePath } from "next/cache";
+  import prisma from "@/lib/prisma";
+  import { auth, currentUser} from "@clerk/nextjs/server";
+  import { revalidatePath } from "next/cache";
 
 export async function syncUser() {
     try {
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+"use client";
 
-function PostCard() {
+import { createComment, deletePost, getPosts, toggleLike } from "@/actions/post.action";
+import { SignInButton, useUser } from "@clerk/nextjs";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { Card, CardContent } from "./ui/card";
+import Link from "next/link";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { formatDistanceToNow } from "date-fns";
+import { DeleteAlertDialog } from "./DeleteAlertDialog";
+import { Button } from "./ui/button";
+import { HeartIcon, LogInIcon, MessageCircleIcon, SendIcon } from "lucide-react";
+import { Textarea } from "./ui/textarea";
+
+type Posts = Awaited<ReturnType<typeof getPosts>>;
+type Post = Posts[number];
+
+function PostCard({ post, dbUserId }: { post: Post; dbUserId: string | null }) {
+  const { user } = useUser();
+  const [newComment, setNewComment] = useState("");
+  const [isCommenting, setIsCommenting] = useState(false);
+  const [isLiking, setIsLiking] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [hasLiked, setHasLiked] = useState(post.likes.some((like) => like.userId === dbUserId));
+  const [optimisticLikes, setOptmisticLikes] = useState(post._count.likes);
+  const [showComments, setShowComments] = useState(false);
+
   return (
-    <div>PostCard</div>
-  )
+    
+  );
 }
-
-export default PostCard
+export default PostCard;
