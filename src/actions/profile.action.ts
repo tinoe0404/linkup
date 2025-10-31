6506c5bb306e4909d153413ed7ecc5407d6c8e82
tinoe0404 <tinoe044@gmail.com>
2025-10-31 10:30:38 +0200

    create getProfileByUsername function

---
+++ src/actions/profile.action.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { getDbUserId } from "./user.action";
+
+export async function getProfileByUsername(username: string) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { username: username },
+      select: {
+        id: true,
+        name: true,
+        username: true,
+        bio: true,
+        image: true,
+        location: true,
+        website: true,
+        createdAt: true,
+        _count: {
+          select: {
+            followers: true,
+            following: true,
+            posts: true,
+          },
+        },
+      },
+    });
+
+    return user;
+  } catch (error) {
+    console.error("Error fetching profile:", error);
+    throw new Error("Failed to fetch profile");
+  }
+}
