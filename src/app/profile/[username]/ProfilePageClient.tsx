42db8befd14bcaf59c9991b837dcbe77187d18fc
tinoe0404 <tinoe044@gmail.com>
2025-10-31 10:53:08 +0200

    create imports for ProfilePageClient

---
+++ src/app/profile/[username]/ProfilePageClient.tsx
@@ -0,0 +1,44 @@
+
+"use client";
+
+import { getProfileByUsername, getUserPosts, updateProfile } from "@/actions/profile.action";
+import { toggleFollow } from "@/actions/user.action";
+import PostCard from "@/components/PostCard";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Textarea } from "@/components/ui/textarea";
+import { SignInButton, useUser } from "@clerk/nextjs";
+import { format } from "date-fns";
+import {
+  CalendarIcon,
+  EditIcon,
+  FileTextIcon,
+  HeartIcon,
+  LinkIcon,
+  MapPinIcon,
+} from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+type User = Awaited<ReturnType<typeof getProfileByUsername>>;
+type Posts = Awaited<ReturnType<typeof getUserPosts>>;
+
+function ProfilePageClient() {
+  return (
+    <div>ProfilePageClient</div>
+  )
+}
+
+export default ProfilePageClient
