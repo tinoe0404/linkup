b45874566a66d2edb36ab5a08e6f16b719a4efb3
tinoe0404 <tinoe044@gmail.com>
2025-10-31 12:08:04 +0200

    added the imageUpload functionality in createPost component

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -54,6 +54,19 @@ function CreatePost() {
                         disabled={isPosting}
                         />
                     </div>
+
+                    {(showImageUpload || imageUrl) && (
+                        <div className="border rounded-lg p-4">
+                        <ImageUpload
+                            endpoint="postImage"
+                            value={imageUrl}
+                            onChange={(url) => {
+                            setImageUrl(url);
+                            if (!url) setShowImageUpload(false);
+                            }}
+                        />
+                        </div>
+                    )}
                     <div className="flex items-center justify-between border-t pt-4">
                         <div className="flex space-x-2">
                         <Button
