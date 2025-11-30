37682ddd50b810b4b133dca89bb839129b996552
tinoe0404 <tinoe044@gmail.com>
2025-11-30 17:42:05 +0200

    fix(uploadthing): rename folder, update imports, and fix build errors

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -3,8 +3,8 @@ import {
         generateUploadDropzone,
     } from "@uploadthing/react";
     
-    // Ensure the path is correct or install the necessary types
-    import type { OurFileRouter } from "../api/uploadthing/core"; // Adjusted path
+    import type { OurFileRouter } from "@/app/api/uploadthing/core";
+
     
     export const UploadButton = generateUploadButton<OurFileRouter>();
     export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
